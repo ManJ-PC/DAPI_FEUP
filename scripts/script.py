146a251a6891a83cdc01eeb78d95c2368c764677
mariosantosprivate <mariosantos.private@gmail.com>
2019-10-14 20:27:19 +0100

    :star: Finished script to create reviews csv file

--- scripts/script.py
+++ scripts/script.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
 import pitchfork
+import json
+import csv
+from progressbar import ProgressBar
 
 artists = []
+storedAlbums = []
 reviews = []
 
 p1 = pitchfork.search('a', '') # the title is autocompleted
@@ -28,37 +32,40 @@ with open(filepath) as fp:
         line = fp.readline()
         cnt += 1
 
+    numberArtists = cnt
+
 # Store reviews in array
     cnt -= 1
+    aux = 1
     while(cnt >= 0):
         artist = artists[cnt]
-        print(artist)
+        print("Parsing artist " + artist + "(" + str(aux) + "/" + str(numberArtists) + ")")
         while(char < 123):
             try:
-                review = pitchfork.search( artist , str(chr(char)))
-                if review not in reviews:
+                tempReview = pitchfork.search( artist , str(chr(char)))
+                album = str(tempReview.album())
+                artist = str(tempReview.artist())
+                score = str(tempReview.score())
+                year = str(tempReview.year())
+                abstract = str(tempReview.abstract())
+                editorial = str(tempReview.editorial())
+                label = str(tempReview.label())
+                
+                review = [album, artist, year, score, abstract, editorial, label]
+
+                if album not in storedAlbums:
+                    storedAlbums.append(album)
                     reviews.append(review)
             except:
                 cnt = cnt
             char += 1
         char = 97
         cnt -= 1
-    
+
+# Write reviews to csv file
+
+    with open('data.csv', 'w') as csvFile:
+        writer = csv.writer(csvFile)
+        writer.writerows(reviews)
+        csvFile.close()
     print(len(reviews))
-    
-
-"""
-p1 = pitchfork.search('a', 'a') # the title is autocompleted
-p2 = pitchfork.search('a', 'b') # the title is autocompleted
-p3 = pitchfork.search('a', 'c') # the title is autocompleted
-p4 = pitchfork.search('a', 'd') # the title is autocompleted
-p5 = pitchfork.search('b', 'a') # the title is autocompleted
-p6 = pitchfork.search('b', 'b') # the title is autocompleted
-
-print(p1.album() + " | " + p1.artist())
-print(p2.album() + " | " + p2.artist())
-print(p3.album() + " | " + p3.artist())
-print(p4.album() + " | " + p4.artist())
-print(p5.album() + " | " + p5.artist())
-print(p6.album() + " | " + p6.artist())
-"""
